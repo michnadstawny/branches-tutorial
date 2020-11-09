e78913f621cb8e0d4ac4154d6254c637d2d226e5
Michal Nadstawny <michnadstawny@gmail.com>
2020-11-09 18:50:59 +0100

    ExploreIf()

--- Program.cs
+++ Program.cs
@@ -4,12 +4,41 @@ namespace BranchesAndLoops
 {
     class Program
     {
-        static void Main(string[] args)
+        static void ExploreIf()
         {
             int a = 5;
-            int b = 6;
+            int b = 3;
             if (a + b > 10)
             Console.WriteLine("The answer is greater than 10.");
+            else
+            Console.WriteLine("The answer is not greater than 10");
+
+            int c = 4;
+            if ((a + b + c > 10) && (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("And the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("Or the first number is not equal to the second");
+            }
+            if ((a + b + c > 10) || (a == b))
+            {
+                Console.WriteLine("The answer is greater than 10");
+                Console.WriteLine("Or the first number is equal to the second");
+            }
+            else
+            {
+                Console.WriteLine("The answer is not greater than 10");
+                Console.WriteLine("And the first number is not equal to the second");
+            }
+        }
+        static void Main(string[] args)
+        {
+            ExploreIf();
+            
         }
     }
 }
